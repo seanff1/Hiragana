ac624539fbb0edd65726682dc67b79df55257f60
seanff1 <seanff1@hotmail.co.uk>
2024-04-01 18:23:50 +0100

    random and answer box

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 import hiraganaCollection from "./helpers/hiragana_collection.js";
 
-console.log(hiraganaCollection);
+const length = hiraganaCollection.length;
+const text = hiraganaCollection[0][0];
+
+const textContainer = document.getElementById("textContainer");
+const answerContainer = document.getElementById("answerContainer");
+textContainer.textContent = text;
+
+let randomNumber;
+
+function generateRandomHiragana() {
+  randomNumber = Math.floor(Math.random() * length);
+  console.log(randomNumber);
+  textContainer.textContent = hiraganaCollection[randomNumber][1];
+}
+const randomButton = document.getElementById("randomButton");
+randomButton.addEventListener("click", generateRandomHiragana);
+
+function getAnswer() {
+  answerContainer.textContent = hiraganaCollection[randomNumber][0];
+}
+const answerButton = document.getElementById("answerButton");
+answerButton.addEventListener("click", getAnswer);
